a310c89b9358c035c3c3441275cbca2d5e6d1180
Prabhat BK <prabhatbk4@gmail.com>
2024-03-06 17:30:18 +0545

    modified about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,17 @@ import userTitle from "../hooks/userTitle";
 
 const About = () => {
   userTitle("About | Page");
-  return <div>This is About Page</div>;
+  return (
+    <div>
+      <div className="heading w-[28%] mt-24 m-auto">
+        <img src="/123.jpg" alt="" className="w-[80%] rounded-xl m-auto" />
+        <div className="text mt-2 w-2/3 m-auto">
+          <h1 className="font-bold text-3xl z-10">Prabhat Bishwakarma
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
